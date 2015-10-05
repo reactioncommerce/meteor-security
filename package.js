ca12c03355ff8441f0fa35807d8b7ee1eadc4e1c
Eric Dobbertin <eric@dairystatedesigns.com>
2015-10-05 15:21:43 -0500

    Bump version and publish

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "ongoworks:security",
   summary: "Logical security for client-originated MongoDB collection operations",
-  version: "1.2.0",
+  version: "1.3.0",
   git: "https://github.com/ongoworks/meteor-security.git"
 });
 
